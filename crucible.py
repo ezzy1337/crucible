e13131bf2b3f26a926f6950883742ebd533cad3c
Derek Drummond <ddrummond@cylance.com>
2020-07-21 08:41:54 -0500

    Proof of concept script

--- crucible.py
+++ crucible.py
@@ -1,27 +1,29 @@
+import click
 import locust
 from locust.env import Environment
 from yaml import load
 
-import tests
-
+import locust_factory
 # TODO workout how to run locust in distributed mode.
 
-
-if __name__ == '__main__':
-    #TODO parse the yaml file nows
-    with open('sandbox/openapiv3.yml', 'r') as spec_f:
+# OMFG The Proof of concept is done....it's time for architectural decisions and tests
+@click.command()
+@click.option('--spec-file', '-s', required=True, type=click.Path(exists=True), help="Path to OpenAPIv3 spec file")
+def crucible(spec_file):
+    with open(spec_file, 'r') as spec_f:
         raw_spec = spec_f.read()
         spec = load(raw_spec)
 
-    # Sweet! I can dynamically set the host by attaching it to the class.
-    # Won't do me good with test scenarios if I have to split up users into multiple
-    # classes since they are pass by reference.
-    tests.CrucibleUser.host = 'http://localhost:8000'
+    test_classes = []
+    for route in spec['paths']:
+        actions = [a for a in spec['paths'][route]]
+        test_classes.append(locust_factory.create_route_locusts(route, actions[0]))
+
     env = Environment(
         # TODO figure out what user_classes expects here...but I think this is correct
-        user_classes=[tests.CrucibleUser], # TODO there's a catch-22 here if Crucible is going to write these classes then call this. Actually it's not to bad since I get to define when this is called.
+        user_classes=test_classes,
         events=locust.events,
-        # host='http://localhost:8000'
+        host='http://localhost:8000'
     )
 
     runner = env.create_local_runner()
@@ -31,3 +33,7 @@ if __name__ == '__main__':
     runner.start(num_users, hatch_rate)
     env.runner.greenlet.join()
     print("runner started")
+
+
+if __name__ == '__main__':
+    crucible()
